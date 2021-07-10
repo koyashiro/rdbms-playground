c0f7a2aa7d8e2b7be542d265d5c59ff30624b70a
koyashiro <develop@koyashiro.net>
2021-07-10 21:05:44 +0900

    feat: implement prototype playground

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Toolbar from "./components/Toolbar";
 import Playground from "./components/Playground";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Toolbar />
       <Playground />
     </div>
   );
