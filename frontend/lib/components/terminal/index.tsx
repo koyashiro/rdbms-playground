c7a33b448ae0260a087e85b775715aeb1556c1ef
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-09-28 03:43:11 +0900

    Add Terminal-like interface prototype

---
+++ frontend/lib/components/terminal/index.tsx
@@ -0,0 +1,74 @@
+import {
+  ChangeEventHandler,
+  createRef,
+  FC,
+  KeyboardEventHandler,
+  TextareaHTMLAttributes,
+  useState,
+} from "react";
+
+type Props = {
+  command: (cmd: string) => Promise<string>;
+};
+
+const defaultHistory = `
+Welcome to Postgres Playground!
+`;
+
+const Terminal: FC<Props> = (props) => {
+  const [history, setHistory] = useState<string>(defaultHistory);
+  const [textareaContent, setTextareaContent] = useState<string>("");
+  const [textareaRows, setTextareaRows] = useState<number>(1);
+
+  const focusRef = createRef<HTMLTextAreaElement>();
+
+  const addHistory = (line: string) => {
+    setHistory(history + line);
+  };
+
+  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setTextareaContent(e.currentTarget.value);
+  };
+
+  const handleKeyPress: KeyboardEventHandler<HTMLTextAreaElement> = async (
+    e
+  ) => {
+    switch (e.key) {
+      case "Enter":
+        if (e.shiftKey) {
+          setTextareaRows(textareaRows + 1);
+        } else {
+          e.preventDefault();
+
+          const res = await props.command(textareaContent);
+          addHistory(`> ${textareaContent}\n${res}\n`);
+
+          setTextareaContent("");
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-col w-full h-full">
+        <pre>{history}</pre>
+        <div className="flex w-full">
+          <pre className="flex-initial">{"> "}</pre>
+          <textarea
+            className="flex-auto outline-none"
+            rows={textareaRows}
+            onChange={handleChange}
+            onKeyPress={handleKeyPress}
+            value={textareaContent}
+            ref={focusRef}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Terminal;
