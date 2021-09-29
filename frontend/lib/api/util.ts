fa542bcbff7ad8a7c06d5c030121285915dadf9d
azarashi <me@azr.sh>
2021-09-29 12:22:07 +0900

    refactor: move API hostname to runtime config (#10)
    * Add top page
    
    * Add Terminal-like interface prototype
    
    * Add terminal-like auto-scroll
    
    * Fix input confirm method
    
    * Fix button style
    
    * Chage terminal color
    
    * Fix font family
    
    * Fix style bugs
    
    * Add terminal history
    
    * Add API_HOST_URI to runtime config
    
    * fixup! Add API_HOST_URI to runtime config

--- frontend/lib/api/util.ts
+++ frontend/lib/api/util.ts
@@ -1,19 +1,22 @@
-type HttpMethod = "GET" | "POST" | "DELETE";
-const Protcol = "https://";
+import getConfig from "next/config";
 
-const API_ENDPOINT_HOST = "";
+type HttpMethod = "GET" | "POST" | "DELETE";
 
 export const fetchApi = async (
   method: HttpMethod,
   endpoint: string,
-  { parameter, body }: { parameter?: any; body?: any }
+  {
+    parameter,
+    body,
+  }: { parameter?: Record<string, string>; body?: Record<string, string> }
 ): Promise<Response> => {
-  if (!API_ENDPOINT_HOST) throw new Error();
+  const { publicRuntimeConfig } = getConfig();
+  const apiHostUri = publicRuntimeConfig.apiHostUri;
 
   const parameterString = parameter
     ? "?" + new URLSearchParams(parameter).toString()
     : "";
-  const url = Protcol + API_ENDPOINT_HOST + endpoint + parameterString;
+  const url = apiHostUri + endpoint + parameterString;
   return await fetch(url, {
     method: method,
     headers: { "Content-Type": "application/json" },
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const validateNotEmptyEnv = (source) => {
+  if (source != null && source !== "") {
+    return source;
+  } else {
+    throw new Error("Environment variable validation failed");
+  }
+};
+
 module.exports = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    apiHostUri: validateNotEmptyEnv(process.env.API_HOST_URI),
+  },
 };
